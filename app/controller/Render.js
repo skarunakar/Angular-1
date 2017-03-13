690635f5b25bd2674bc35a01b38059317eb3bbee
skarunakar <shreya_karunakar@intuit.com>
2017-03-13 11:37:08 +0530

    after first poc

--- app/controller/Render.js
+++ app/controller/Render.js
@@ -5,17 +5,39 @@
 
      var myCtrl = function($scope,$http) {
 
-
-
-        $scope.requestJson = function (dataSet) {
-                    $http.get("http://127.0.0.1:8000?dataSet="+dataSet)
+    $scope.requestJson = function (dataSet) {
+        window.performance.mark("startLoading");
+        $http.get("http://127.0.0.1:8000?dataSet="+dataSet)
                         .then(function (response) {
+                            $scope.id = dataSet;
                             $scope.myResponse = response.data;
-                            $scope.myResponse = $scope.myResponse.var;
-                   });
+                            $scope.myResponse = $scope.myResponse;
+                            window.performance.mark("startLoad");
+         });
         }
-     }
+    $scope.update = function (dataSet) {
+             window.performance.mark("startLoading");
+             $http.get("http://127.0.0.1:3001?dataSet="+dataSet)
+                 .then(function (response) {
+                     $scope.id = dataSet;
+                     $scope.myResponse = response.data;
+                     $scope.myResponse = $scope.myResponse;
+                     window.performance.mark("startLoad");
+                 });
+         }
+    $scope.$watch("assignments", function (value) {
+             if (window.performance.getEntriesByName("startLoad").length != 0) {
+                 window.performance.mark("endLoading");
+                 window.performance.measure("name", "startLoad", "endLoading");
+                 var array = window.performance.getEntriesByType('measure');
+                 console.log(array[array.length - 1].duration);
+                 window.performance.clearMarks();
+                 window.performance.clearMeasures();
+             }
 
+         });
+
+     };
      app.controller("myCtrl", ["$scope","$http",myCtrl]);
 
 }());
